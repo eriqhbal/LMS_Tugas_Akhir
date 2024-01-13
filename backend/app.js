5b0f92cfc8b763113913b4f14e82dbf3c44a6393
Zuleriqhbal <eriqhbal@gmail.com>
2024-01-13 18:38:53 +0700

    fixing

--- backend/app.js
+++ backend/app.js
@@ -14,12 +14,11 @@ const port = process.env.PORT || 4000;
 // Connect to vercel
 app.use(
   cors({
-    origin: ["lms-tugas-akhir-3raa.vercel.app/"],
+    origin: [""],
     methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
     credentials: true
   })
 );
-app.use(express.json());
 
 // Import Route
 const login = require("./routes/login");
